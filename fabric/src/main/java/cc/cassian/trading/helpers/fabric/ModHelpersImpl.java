3886bb3a3ba9002b9a8b35cee3f7f85ae46d511f
g <kensho.tsurusaki@gmail.com>
2025-09-01 01:32:02 +0900

    RebindQuickSwap mod integration

--- fabric/src/main/java/cc/cassian/trading/helpers/fabric/ModHelpersImpl.java
+++ fabric/src/main/java/cc/cassian/trading/helpers/fabric/ModHelpersImpl.java
@@ -1,9 +1,16 @@
 package cc.cassian.trading.helpers.fabric;
 
 import net.fabricmc.loader.api.FabricLoader;
+import walksy.quickswaprebinder.RebindQuickSwapMod;
 
 public class ModHelpersImpl {
     public static boolean clothConfigInstalled() {
         return FabricLoader.getInstance().isModLoaded("cloth-config");
+    }
+    public static boolean shouldQuickSwap() {
+        if (FabricLoader.getInstance().isModLoaded("rebindquickswap")) {
+            return RebindQuickSwapMod.shouldQuickSwap();
+        }
+        return net.minecraft.client.gui.screen.Screen.hasShiftDown();
     }
 }
