b7a70322a45e603412f62d2013d6ad27127ece3c
ge9 <ge9@users.noreply.github.com>
2025-10-05 13:29:54 +0900

    RebindQuickSwap mod integration(1.21.9)

--- fabric/src/main/java/cc/cassian/trading/helpers/fabric/ModHelpersImpl.java
+++ fabric/src/main/java/cc/cassian/trading/helpers/fabric/ModHelpersImpl.java
@@ -1,9 +1,17 @@
 package cc.cassian.trading.helpers.fabric;
 
 import net.fabricmc.loader.api.FabricLoader;
+import net.minecraft.client.MinecraftClient;
+import walksy.quickswaprebinder.RebindQuickSwapMod;
 
 public class ModHelpersImpl {
     public static boolean clothConfigInstalled() {
         return FabricLoader.getInstance().isModLoaded("cloth-config");
+    }
+    public static boolean shouldQuickSwap() {
+        if (FabricLoader.getInstance().isModLoaded("rebindquickswap")) {
+            return RebindQuickSwapMod.shouldQuickSwap();
+        }
+        return MinecraftClient.getInstance().isShiftPressed();
     }
 }
